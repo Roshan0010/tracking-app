00088325f3cdd48f8588f1b2f9edb5f327c5fcd2
Roshan0010 <iamroshan6417@gmail.com>
2024-02-01 19:39:44 +0530

    redme update

--- README.md
+++ README.md
@@ -34,3 +34,30 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+#process
+1st
+//next
+npm i nvm
+nvm i 20
+nvm use 20
+npm i
+npm run dev
+
+2nd
+//hardhat server
+naya termial
+npx hardhat node
+
+3rd
+naya termial
+// harhat deloy contract
+npx hardhat run --network localhost scripts/deploy.js
+
+make sure to add localhost networks in
+metamask by :https://www.youtube.com/watch?v=wDueg2_aOTA
+then add localhost accounts by adding private key given in 2rd step in response in terminal :
+by going to metamask and doing steps given
+1>click in account
+2>add acount or hardware
+3>import account paste the private key of the any 20 accounts given in the terminal of 2nd
--- src/Components/itemTable.jsx
+++ src/Components/itemTable.jsx
@@ -2,24 +2,23 @@ import React from "react";
 
 const ItemTableTable = ({ allShipmentsData, rowArray }) => {
 	// console.log(allShipmentsData);
-	const statusValue=["PENDING","IN_TRANSIT","DELIVERED"];
+	const statusValue = ["PENDING", "IN_TRANSIT", "DELIVERED"];
 	const convertTime = (time) => {
 		const newTime = new Date(time);
-		
+
 		const formattedTime = new Intl.DateTimeFormat("en-US", {
 			year: "numeric",
 			month: "2-digit",
 			day: "2-digit",
 		}).format(newTime);
-	
+
 		return formattedTime;
 	};
-	
+
 	const Row = () => {
 		let serialNumber = 1;
 		return (
 			<div className=" h-full w-full">
-
 				{allShipmentsData?.map((shipment, idx) => (
 					<div className="flex">
 						<div className="w-[12.5%]">{shipment.sender.slice(0, 15)}..</div>
@@ -27,15 +26,9 @@ const ItemTableTable = ({ allShipmentsData, rowArray }) => {
 						<div className="w-[12.5%]">{convertTime(shipment.pickupTime)}</div>
 						<div className="w-[12.5%]">{shipment.distance}Km</div>
 						<div className="w-[12.5%]">{shipment.price}..</div>
-						<div className="w-[12.5%]">
-						{convertTime(shipment.deliverTime)}
-						</div>
-						<div className="w-[12.5%]">
-							{shipment.isPaid ? "Yes" : "No"}
-						</div>
-						<div className="w-[12.5]">
-							{statusValue[shipment.status] }
-						</div>
+						<div className="w-[12.5%]">{convertTime(shipment.deliverTime)}</div>
+						<div className="w-[12.5%]">{shipment.isPaid ? "Yes" : "No"}</div>
+						<div className="w-[12.5]">{statusValue[shipment.status]}</div>
 					</div>
 				))}
 			</div>
@@ -48,8 +41,10 @@ const ItemTableTable = ({ allShipmentsData, rowArray }) => {
 		>
 			<div className="w-[100%]  ">
 				<div className="flex">
-					{rowArray.map((item,idx) => (
-						<div key={idx} className="w-[12.5%] px-1 py-2  ">{item}</div>
+					{rowArray.map((item, idx) => (
+						<div key={idx} className="w-[12.5%] px-1 py-2  ">
+							{item}
+						</div>
 					))}
 				</div>
 				<Row className="bg-yellow-400 overflow-y-scroll h-full w-full " />
